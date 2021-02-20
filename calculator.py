17fef266be2d5490da181993234045b1dfd06b25
ib-dev-cpp <79328676+ib-dev-cpp@users.noreply.github.com>
2021-02-20 18:21:29 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,6 +10,6 @@ f = open('data.txt') # open the data.txt in read mode
 
 exec(f.read()) # executing the code inside the data.txt file
 
-print('The result is %d' % result) # printing the result
+print('The result is %f' % float(result)) # printing the result
 
 f.close() # closing the file
