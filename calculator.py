18b8a443730db958a3322011251ddd0032ce549f
ib-dev-cpp <79328676+ib-dev-cpp@users.noreply.github.com>
2021-02-21 10:37:26 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,5 @@
-f = open('data.txt', 'w') # opening the data.txt file in write mode
-
 i = input('put your expresion here\n') # taking the expresion from the user 
 
-f.write("result = " + i) # writing in the data.txt file 
-
-f.close() # closing the file
-
-f = open('data.txt') # open the data.txt in read mode
-
-exec(f.read()) # executing the code inside the data.txt file
+exec("result = " + i) # executing the code inside the ()
 
 print('The result is %f' % float(result)) # printing the result
-
-f.close() # closing the file
